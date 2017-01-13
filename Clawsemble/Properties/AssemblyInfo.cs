7cfdaa023d87f574fd3dc638ee7d9f9016a03b97
Benedikt Müssig <benedikt.muessig@gmail.com>
2017-01-13 22:13:30 +0100

    Fixed copyright

--- Clawsemble/Properties/AssemblyInfo.cs
+++ Clawsemble/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Clawsemble")]
-[assembly: AssemblyCopyright("Copyright © Benedikt Müssig, 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017, Benedikt Müssig")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
